35b1fb7176758c57ecc999fc5fd450410d559aa8
lll123963 <128034573+lll123963@users.noreply.github.com>
2025-06-03 22:45:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
 
-login_url = 'https://ikuuu.art/auth/login'
-check_url = 'https://ikuuu.art/user/checkin'
-info_url = 'https://ikuuu.art/user/profile'
+login_url = 'https://ikuuu.one/auth/login'
+check_url = 'https://ikuuu.one/user/checkin'
+info_url = 'https://ikuuu.onne/user/profile'
 
 header = {
         'origin': 'https://ikuuu.art',
